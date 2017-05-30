6d850ce718790510a06d34b82397e9df23a20420
Wojciech Sierakowski <wsierakowski@gmail.com>
2017-05-30 22:50:02 +0100

    Add test for content provider local git repo - get article dir contents functionality

--- app/article-importer/cp-local-git-repo.js
+++ app/article-importer/cp-local-git-repo.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const glob = require('glob');
+const async = require('async');
 
 const conf = require('../config');
 const ContentProviderInterface = require('./cp-interface');
@@ -17,19 +18,35 @@ class ContentProviderLocalGitRepo extends ContentProviderInterface {
     const exts = confFileExtsList.join(',');
 
     const globPath = path.join(conf.app.paths.localRepoDir, '**', `?(${filenames})`) + `.{${exts}}`;
-    console.log('**>', globPath)
 
     // 1. Search through all directories and identify those that have conf files.
-    glob(globPath, (err, confPaths) => {
-      if (err) return cb(err);
+    glob(globPath, (gperr, confPaths) => {
+      if (gperr) return cb(gperr);
 
       // 2. Go through each directory identified as article dir and list the files inside.
-      const ret = [];
-      console.log('---->', confPaths);
-      //async.each(confPaths, (confPath, cb1) => {
-        //const globArticleDir = path.join(confPaths);
-      //});
+      //const ret = [];
+      async.map(confPaths, getArticleContent, (cferr, res) => {
+        if (cferr) return cb(cferr);
+        cb(null, res);
+      });
     });
+
+    function getArticleContent(confPath, cb1) {
+      const dirContent = {
+        dirPath: path.dirname(confPath),
+        confFile: path.basename(confPath)
+      };
+      const articleGlobPath = path.join(dirContent.dirPath, '**', '*.*');
+      glob(articleGlobPath, (agerr, articleFiles) => {
+        if (agerr) return cb1(agerr);
+        articleFiles = articleFiles
+          .map(file => path.relative(dirContent.dirPath, file))
+          .filter(file => file !== dirContent.confFile);
+        dirContent.staticFiles = articleFiles;
+        //ret.push(dirContent);
+        cb1(null, dirContent);
+      });
+    };
   }
 }
 
--- test/article-importer/cp-local-git-repo.test.js
+++ test/article-importer/cp-local-git-repo.test.js
@@ -1,4 +1,4 @@
-//const test = require('tape');
+const test = require('tape');
 const path = require('path');
 const proxyquire = require('proxyquire');
 
@@ -13,7 +13,22 @@ const confStub = {
 const CPLocalGitRepo = proxyquire('../../app/article-importer/cp-local-git-repo', {
   '../config': confStub
 });
-//const CPLocalGitRepo = require('./app/article-importer/cp-local-git-repo');
+
 const cplgr = new CPLocalGitRepo();
 
-cplgr.getArticleDirsContent(['conf', 'config'], ['json', 'yaml']);
+test('ContentProvider.getArticleDirsContent should return correct objects', function(t) {
+  const expected = require('./cp-local-git-repo.fixture.js');
+
+  cplgr.getArticleDirsContent(['conf', 'config'], ['json', 'yaml'], (err, result) => {
+    // todo - error?
+
+    result.forEach(item => item.dirPath = path.relative(confStub.app.paths.localRepoDir, item.dirPath));
+    // console.log('==---- results ---==');
+    // console.log('err', err);
+    // console.log('res', result);
+
+     t.deepEqual(result, expected);
+    //t.equal(true, true);
+    t.end();
+  });
+});
